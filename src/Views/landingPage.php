aea6a6177f553d72c22cb4b0141f62d787e9f1a5
danyealmateen <danyeal.mateen@hotmail.com>
2026-02-06 22:37:06 +0100

    new pic

--- src/Views/landingPage.php
+++ src/Views/landingPage.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Thirst Order</title>
+    <link rel="icon" type="image/svg+xml" href="/images/favicon.svg">
     <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
     <div class="hud-frame" aria-hidden="true"></div>
-    <p>landing page</p>
+    <h1>Testing Title The Thirst Order</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ex amet sapiente illo facilis quaerat quas magnam! Mollitia placeat dolorem quibusdam commodi odit quasi quidem voluptates. Necessitatibus reiciendis suscipit obcaecati, nihil tenetur et dolores nesciunt, quam fugiat ipsam qui odit tempore voluptatum maxime est. Suscipit rem quidem culpa maiores quasi!</p>
+
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ex amet sapiente illo facilis quaerat quas magnam! Mollitia placeat dolorem quibusdam commodi odit quasi quidem voluptates. Necessitatibus reiciendis suscipit obcaecati, nihil tenetur et dolores nesciunt, quam fugiat ipsam qui odit tempore voluptatum maxime est. Suscipit rem quidem culpa maiores quasi!</p>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ex amet sapiente illo facilis quaerat quas magnam! Mollitia placeat dolorem quibusdam commodi odit quasi quidem voluptates. Necessitatibus reiciendis suscipit obcaecati, nihil tenetur et dolores nesciunt, quam fugiat ipsam qui odit tempore voluptatum maxime est. Suscipit rem quidem culpa maiores quasi!</p>
 </body>
 </html>
